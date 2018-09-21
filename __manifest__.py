236fae291b5390dcc7370e53675c55143ca03259
owlvano <owlvano@gmail.com>
2018-09-21 11:45:58 +0300

    add porduct dependency

--- __manifest__.py
+++ __manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'decimal_precision'],
+    'depends': ['base', 'product', 'decimal_precision'],
 
     # always loaded
     'data': [
