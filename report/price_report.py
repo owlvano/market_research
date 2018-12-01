762810939ab2903d9596e56490e8fdb154fa051e
owlvano <owlvano@gmail.com>
2018-12-01 15:15:44 +0200

    upd product selection in report query

--- report/price_report.py
+++ report/price_report.py
@@ -14,8 +14,7 @@ class PriceReport(models.Model):
     client_id = fields.Many2one('res.partner', "Client", readonly=True)
     assigned_user_id = fields.Many2one('res.users', "Assigned User", readonly=True)
 
-    product_id = fields.Many2one('product.product', "Product", readonly=True)    
-
+    product_id = fields.Many2one('product.product', "Product", readonly=True)
 
     def _query_price_measurements(self):
         query_str = """
@@ -24,19 +23,19 @@ class PriceReport(models.Model):
                 NULLIF(pm.measured_price, 0) as measured_price,
                 t.assigned_user_id as assigned_user_id,
                 t.client_id as client_id,
-                p.product_id as product_id,
+                p.id as product_id,
                 t.measurement_order_id as measurement_order_id
             FROM (
                 market_research_price_measurement pm
                     left join market_research_tradepoint_order t on (pm.tradepoint_order_id=t.id)
-                    left join market_research_product p on (pm.product_id=p.product_id)
+                    left join product_product p on (pm.product_id=p.id)
             )
             GROUP BY
                 pm.create_date,
                 pm.measured_price,
                 t.assigned_user_id,
                 t.client_id,
-                p.product_id,
+                p.id,
                 t.measurement_order_id
         """ 
         return query_str
@@ -48,18 +47,19 @@ class PriceReport(models.Model):
                 NULLIF(p.price, 0) as measured_price,
                 m.responsible_id as assigned_user_id,
                 m.partner_id as client_id,
-                p.product_id as product_id,
+                pp.id as product_id,
                 m.id as measurement_order_id
             FROM (
                 market_research_product p
                     left join market_research_measurement_order m on (p.measurement_order_id=m.id)
+                    left join product_product pp on (p.product_id=pp.id)
             )
             GROUP BY 
                     p.create_date,
                     p.price,
                     m.responsible_id,
                     m.partner_id,
-                    p.product_id,
+                    pp.id,
                     m.id
         """
         return query_str   
